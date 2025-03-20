b49e708cdcab63e533987a5941762653f1cc364d
acaballee <acaballe@iesebre.com>
2025-03-20 10:00:35 +0100

    test

---
+++ pt6_refactoring_enunciat-main (2)/pt6_refactoring_enunciat-main/refactoring/src/test/java/org/entdes/TaulerServiceTest.java
@@ -0,0 +1,34 @@
+package org.entdes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TaulerServiceTest {
+
+    private TaulerService taulerService;
+
+    @BeforeEach
+    void setUp() {
+
+        taulerService = new TaulerService();
+    }
+
+    @Test
+    void testComprovarGuanyadorX() {
+        taulerService.tractarClicCasella(0, 0);//X
+        taulerService.tractarClicCasella(0, 1);//O
+        taulerService.tractarClicCasella(0, 2);//X
+        taulerService.tractarClicCasella(1, 1);//O
+        taulerService.tractarClicCasella(1, 0);//X
+        taulerService.tractarClicCasella(1, 2);//O
+        taulerService.tractarClicCasella(2, 0);//X
+        
+        assertTrue(taulerService.isGameOver());
+        assertEquals("X", taulerService.getGuanyador());
+    }
+
+
+}
